2e12d6553980336be8b488c4d379945f46233e4e
Edvinas <test@test.com>
2017-05-10 19:31:11 +0300

    Second submit, changed class variables data structure, added getters and setters

--- ReportCard.java
+++ ReportCard.java
@@ -13,132 +13,43 @@ import java.util.Map;
  * It can also calculate average subject grade and average total grade.
  */
 public class ReportCard {
-    /** @param gradesMap HashMap data structure to hold subjects and grades*/
-    Map<String, ArrayList<Integer>> gradesMap = 
-            new HashMap<String, ArrayList<Integer>>();
-    /**
-     * ReportCard constructor that builds object from
-     * String and ArrayList<Integer>
-     */
-    public ReportCard(String subject, ArrayList<Integer> grades) {
-        if (gradesMap.get(subject) == null) 
-            this.gradesMap.put(subject, grades);
-        else return;
+    
+    // Declaring class variables accessible only in the class scope
+    private String subject;                                      
+    private ArrayList<Integer> grades = new ArrayList<Integer>();
+    
+    /* ReportCard constructor that builds object from
+       String subject and ArrayList<Integer> grades */
+    public ReportCard(String subject, ArrayList<Integer> grades){
+        this.subject = subject;
+        this.grades = grades;
     }
-    /**
-     * Empty ReportCard object constructor if user wants to set it up later.
-     */
+    
+    // Empty ReportCard object constructor if user wants to set up object later.
     public ReportCard(){};
-    /**
-     * ReportCard constructor that builds object from HashMap.
-     * @param gradesMap HashMap data structure to build ReportCard object.
-     */
-    public ReportCard(Map<String, ArrayList<Integer>> gradesMap) {
-        this.gradesMap = gradesMap;
-    }
-    /**
-     * Sets subject and it's grades from parameters.
-     */
-    public void setSubjectAndGrades(String subject, ArrayList<Integer> grades){
-        if (gradesMap.get(subject) == null) 
-            this.gradesMap.put(subject, grades);
-        else return;
-    }
-    /**
-     * Returns ArrayList<String> of all the subjects in ReportCard.
-     */
-    public ArrayList<String> getSubjects(){
-        ArrayList<String> subjects = new ArrayList<String>();
-        if(!gradesMap.isEmpty())
-            for (Map.Entry<String, ArrayList<Integer>> ee : gradesMap.entrySet()) 
-                subjects.add(ee.getKey());
-        return subjects;
-    }
-    /**
-     * Returns ArrayList<Integer> of all the grades of subject
-     * specified in the parameter.
-     */
-    public ArrayList<Integer> getGrades(String subject){
-        ArrayList<Integer> grades = new ArrayList<Integer>();
-        if(!gradesMap.isEmpty())
-            for (Map.Entry<String, ArrayList<Integer>> ee : gradesMap.entrySet()) 
-                if(ee.getKey() == subject){
-                    List<Integer> values = ee.getValue();
-                    for (int i = 0; i < values.size(); i++)
-                        grades.add(values.get(i));
-                }
-        return grades;
-    }    
-    /**
-     * Calculates and returns double value of average grades from subject 
-     * specified in the parameter
-     */
-    public double getSubjectAverage(String subject){
+
+    // Returns class variables to outside of the class scope
+    public String getSubject() {return subject;}
+    public ArrayList<Integer> getGrades() { return grades;}
+    
+    // Sets class variables
+    public void setSubject(String subject) {this.subject = subject;}
+    public void setGrades(ArrayList<Integer> grades) {this.grades = grades;}
+    
+    // Calculates and returns double value of average grades of subject
+    public double getAverageGrade(){
         double sumOfGrades = 0.00;
-        double totalGrades = 0.00;
-        double average = 0.00;
-        if(!gradesMap.isEmpty())
-            for (Map.Entry<String, ArrayList<Integer>> ee : gradesMap.entrySet()) 
-                if(ee.getKey() == subject){
-                    List<Integer> values = ee.getValue();
-                    totalGrades = values.size();
-                    for (int i = 0; i < values.size(); i++)
-                        sumOfGrades += values.get(i);
-                }
-        average = sumOfGrades/totalGrades;
-        return average;
+        for (int i = 0; i < grades.size(); i++)
+            sumOfGrades += grades.get(i);
+        return sumOfGrades/grades.size();
     }
-    /**
-     * Calculates and returns double value of total average grade from all
-     * subjects.
-     */
-    public double getTotalAverage(){
-        double sumOfAverages = 0.00;
-        double totalAverages = 0.00;
-        double average = 0.00;
-        if(!gradesMap.isEmpty())
-            for (Map.Entry<String, ArrayList<Integer>> ee : gradesMap.entrySet()) {
-                double f = 0.5;
-                double rounded = f * Math.round(getSubjectAverage(ee.getKey())/f);
-                sumOfAverages += rounded;
-                totalAverages++;
-            }
-        average = sumOfAverages/totalAverages;
-        return average;
-    }
-    /**
-     * Prints readable report of subject and it's grades in console.
-     */
-    public void print() {
-        ArrayList<Integer> grades = new ArrayList<Integer>();
-        String subject2;
-        for (Map.Entry<String, ArrayList<Integer>> ee : gradesMap.entrySet()) {
-            String key = ee.getKey();
-            System.out.print(key + " ");
-            List<Integer> values = ee.getValue();
-            for (int i = 0; i < values.size(); i++){
-                System.out.print(values.get(i)+ " ");
-            }
-            System.out.print("\n");
-           }
-    }
-    /**
-     * Returns readable String of subject and it's grades.
-     */
+    
+    // Returns readable String of subject and it's grades.
     @Override
     public String toString() {
-        String toString = "";
-        ArrayList<Integer> grades = new ArrayList<Integer>();
-        String subject2;
-        for (Map.Entry<String, ArrayList<Integer>> ee : gradesMap.entrySet()) {
-            String key = ee.getKey();
-            toString += ee.getKey();
-            List<Integer> values = ee.getValue();
-            for (int i = 0; i < values.size(); i++){
-                toString += " " + values.get(i);
-            }
-            toString += "\n";
-           }
-        return toString;
+        String reportCardString = subject + " ";
+        for (int i = 0; i < grades.size(); i++)
+            reportCardString += grades.get(i) + " ";
+        return reportCardString;
     }
 }
